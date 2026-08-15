3d696f5fb51fe01c06901c82fdeeb491319dc0ac
eshaanatknights <eshaan@knights6901.org>
2026-08-15 17:24:59 -0500

    basic gpio testing

--- code.py
+++ code.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+import board
+import digitalio
+
+import time
+
+switchOutput = digitalio.DigitalInOut(board.GP0)
+switchOutput.direction = digitalio.Direction.OUTPUT
+switchOutput.value = True
+
+switchInput = digitalio.DigitalInOut(board.GP1)
+switchInput.direction = digitalio.Direction.INPUT
+switchInput.pull = digitalio.Pull.DOWN
+
+while True:
+    print(switchInput.value)
+    time.sleep(0.75)
