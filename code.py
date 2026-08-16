4585954ccc7a97b95c7c67a9e08a14c9f3acb4be
NEshaan2438 <neshaanreddy@gmail.com>
2026-08-15 19:50:54 -0500

    added hid libs and testing mouse functionality

--- code.py
+++ code.py
@@ -1,8 +1,14 @@
 import board
 import digitalio
 
+import usb_hid
+from adafruit_hid.mouse import Mouse
+
 import time
 
+USBMouse = Mouse(usb_hid.devices)
+
+# connected pins with a jumper wire that i can unplug/plug in when i want to debug
 switchOutput = digitalio.DigitalInOut(board.GP0)
 switchOutput.direction = digitalio.Direction.OUTPUT
 switchOutput.value = True
@@ -12,5 +18,7 @@ switchInput.direction = digitalio.Direction.INPUT
 switchInput.pull = digitalio.Pull.DOWN
 
 while True:
-    print(not switchInput.value)
+    if switchInput.value:
+        USBMouse.move(x=5, y=5)
+        print(switchInput.value)
     time.sleep(0.75)
